134e5fb66b75aaa87832a7677549b550c81509bf
Barbara Kuofie <barbara.kuofie@gmail.com>
2020-08-28 14:18:32 +0000

    Automatically backed up by Learn

--- lib/intro_to_ruby_hashes_lab.rb
+++ lib/intro_to_ruby_hashes_lab.rb
@@ -5,22 +5,16 @@ end
 
 def my_hash
   # return a valid hash with any key/value pair of your choice
-  {
-    shoe_size: 9
-  }
+  { shoe_size: 9 }
 end
 
 def pioneer
   # return a hash with a key of :name and a corresponding value of 'Grace Hopper'
-  linguist {
-    :name => 'Grace Hopper'
-  }
+  {:name => 'Grace Hopper'}
 end
 
 
 def id_hash_generator(number)
   # return a hash with a key :id assigned to the provided number
-  has2 = {
-    :id => number
-  }
+  {:id => number}
 end
